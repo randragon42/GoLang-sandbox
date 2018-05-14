038c54800a77f652e2cf5b407d3df27b7b7c7c0c
randragon42 <josh.graf1@gmail.com>
2018-05-14 12:46:16 -0500

    Solve sudoku and test passes

--- sudoku/sudoku.go
+++ sudoku/sudoku.go
@@ -1,9 +1,15 @@
 package sudoku
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 )
+
+type Spot struct {
+	row int
+	col int
+}
 
 var puzzle [9][9]int
 
@@ -11,7 +17,7 @@ var puzzle [9][9]int
 func SolveSudoku(input string) string {
 	puzzle = convertToPuzzle(input)
 
-	solvePuzzle()
+	solvePuzzleBruteForce()
 
 	return convertToString(puzzle)
 }
@@ -35,12 +41,16 @@ func convertToPuzzle(input string) [9][9]int {
 	return convertedPuzzle
 }
 
+// Convert an integer array representing a sudoku puzzle into a multiline string
 func convertToString(input [9][9]int) string {
 	convertedPuzzle := ""
 
 	for _, row := range input {
-		for _, num := range row {
-			convertedPuzzle += strconv.Itoa(num) + " "
+		for j, num := range row {
+			convertedPuzzle += strconv.Itoa(num)
+			if j != len(row)-1 {
+				convertedPuzzle += " "
+			}
 		}
 		convertedPuzzle += "\n"
 	}
@@ -48,22 +58,96 @@ func convertToString(input [9][9]int) string {
 	return convertedPuzzle
 }
 
-func solvePuzzle() {
-	// Loop through the puzzle looking for unsolved spaces
+func solvePuzzleBruteForce() {
+	unsolvedSpots := []Spot{}
+	// Loop through the puzzle looking for unsolved spaces and store in slice
+	for i, row := range puzzle {
+		for j, num := range row {
+			if num == 0 {
+				unsolvedSpots = append(unsolvedSpots, Spot{i, j})
+			}
+		}
+	}
 
-	// Check for possible values in an unsolved space
-	// If there is only one, solve that space
-
-	// Check to see if the puzzle is solved, if not, solvePuzzle()
+	solveSpot(unsolvedSpots, 0)
 }
 
-// Find all possible values that a specific spot could be
-func findPossibleValues(row int, col int) {
+// Recursively brute force solutions for the puzzle until we find one or exhaust all possible options
+func solveSpot(unsolvedSpots []Spot, index int) bool {
+	spot := unsolvedSpots[index]
+	// Find all possible values for the index passed in
+	possibleSolutions := findPossibleSolutions(unsolvedSpots[index])
+	// If we are at the last index and there is only one value -> end condition -> return true
+	if index == len(unsolvedSpots)-1 && len(possibleSolutions) == 1 {
+		puzzle[spot.row][spot.col] = possibleSolutions[0]
+		return true
+	}
+
+	// Loop through all of those values and try them
+	for _, possibleSolution := range possibleSolutions {
+		puzzle[spot.row][spot.col] = possibleSolution
+		// Call solveSpot(array, index + 1)
+		// if it returns true, a solution was found
+		if solveSpot(unsolvedSpots, index+1) {
+			return true
+		}
+	}
+
+	// Reset the spot to 0 (spot still unsolved)
+	puzzle[spot.row][spot.col] = 0
+	return false
+
+}
+
+func findPossibleSolutions(spot Spot) []int {
+	// Start with a slice of values 1 through 9
+	possibleSolutions := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+
 	// Check row
+	for _, num := range puzzle[spot.row] {
+		if num != 0 {
+			possibleSolutions = removeFromPossibleSolutions(num, possibleSolutions)
+		}
+	}
+
+	if spot.row == 3 && spot.col == 6 {
+		fmt.Println("Row:", possibleSolutions)
+	}
 
 	// Check column
+	for i := 0; i < 9; i++ {
+		if puzzle[i][spot.col] != 0 {
+			possibleSolutions = removeFromPossibleSolutions(puzzle[i][spot.col], possibleSolutions)
+		}
+	}
 
-	// Check the box it's in
+	if spot.row == 3 && spot.col == 6 {
+		fmt.Println("Col:", possibleSolutions)
+	}
 
-	// Return a list of values that weren't ruled out
+	// Check box
+	x := spot.row - (spot.row % 3)
+	y := spot.col - (spot.col % 3)
+	for i := x; i < x+3; i++ {
+		for j := y; j < y+3; j++ {
+			if puzzle[i][j] != 0 {
+				possibleSolutions = removeFromPossibleSolutions(puzzle[i][j], possibleSolutions)
+			}
+		}
+	}
+
+	if spot.row == 3 && spot.col == 6 {
+		fmt.Println("Box:", possibleSolutions)
+	}
+
+	return possibleSolutions
+}
+
+func removeFromPossibleSolutions(numberToRemove int, possibleSolutions []int) []int {
+	for i, num := range possibleSolutions {
+		if num == numberToRemove {
+			return append(possibleSolutions[:i], possibleSolutions[i+1:]...)
+		}
+	}
+	return possibleSolutions
 }
--- sudoku/sudoku_test.go
+++ sudoku/sudoku_test.go
@@ -21,7 +21,8 @@ func TestSolveSudoku(t *testing.T) {
 8 9 1 2 3 4 5 6 7
 3 4 5 6 7 8 9 1 2
 6 7 8 9 1 2 3 4 5
-9 1 2 3 4 5 6 7 8`
+9 1 2 3 4 5 6 7 8
+`
 
 	solution := SolveSudoku(testProblem)
 
