8e0536cc3a78d7a349d50154ca0e06bbeb59e82a
randragon42 <josh.graf1@gmail.com>
2018-05-15 12:54:14 -0500

    update sudoku solver to expect and return JSON

--- sudoku/sudoku.go
+++ sudoku/sudoku.go
@@ -1,12 +1,13 @@
 package sudoku
 
 import (
-	"strconv"
-	"strings"
+	"encoding/json"
+	"fmt"
 )
 
 type SudokuProblem struct {
-	Problem string
+	Problem [9][9]int
+	Solved  bool
 }
 
 type Spot struct {
@@ -14,57 +15,38 @@ type Spot struct {
 	col int
 }
 
-var puzzle [9][9]int
+var puzzle SudokuProblem
 
 //SolveSudoku solves a 9x9 sudoku puzzle passed as a string with '_' representing blanks
 func SolveSudoku(input string) string {
-	puzzle = convertToPuzzle(input)
+	puzzle = ConvertJsonToPuzzle(input)
 
-	solvePuzzleBruteForce()
+	puzzle.Solved = solvePuzzleBruteForce()
 
-	return convertToString(puzzle)
+	return ConvertPuzzleToJson(puzzle)
 }
 
-// Convert a string into an integer array
-func convertToPuzzle(input string) [9][9]int {
-	var convertedPuzzle [9][9]int
-
-	rows := strings.Split(input, "\n")
-	for i := 0; i < len(rows); i++ {
-		nums := strings.Split(rows[i], " ")
-		for j := 0; j < len(nums); j++ {
-			if nums[j] != "_" {
-				num, err := strconv.Atoi(nums[j])
-				if err == nil {
-					convertedPuzzle[i][j] = num
-				}
-			}
-		}
+func ConvertJsonToPuzzle(input string) SudokuProblem {
+	sudokuProblem := SudokuProblem{}
+	err := json.Unmarshal([]byte(input), &sudokuProblem)
+	if err != nil {
+		fmt.Println(err.Error())
 	}
-	return convertedPuzzle
+	return sudokuProblem
 }
 
-// Convert an integer array representing a sudoku puzzle into a multiline string
-func convertToString(input [9][9]int) string {
-	convertedPuzzle := ""
-
-	for _, row := range input {
-		for j, num := range row {
-			convertedPuzzle += strconv.Itoa(num)
-			if j != len(row)-1 {
-				convertedPuzzle += " "
-			}
-		}
-		convertedPuzzle += "\n"
+func ConvertPuzzleToJson(input SudokuProblem) string {
+	problemJson, err := json.Marshal(input)
+	if err != nil {
+		fmt.Println(err.Error())
 	}
-
-	return convertedPuzzle
+	return string(problemJson)
 }
 
-func solvePuzzleBruteForce() {
+func solvePuzzleBruteForce() bool {
 	unsolvedSpots := []Spot{}
 	// Loop through the puzzle looking for unsolved spaces and store in slice
-	for i, row := range puzzle {
+	for i, row := range puzzle.Problem {
 		for j, num := range row {
 			if num == 0 {
 				unsolvedSpots = append(unsolvedSpots, Spot{i, j})
@@ -72,7 +54,7 @@ func solvePuzzleBruteForce() {
 		}
 	}
 
-	solveSpot(unsolvedSpots, 0)
+	return solveSpot(unsolvedSpots, 0)
 }
 
 // Recursively brute force solutions for the puzzle until we find one or exhaust all possible options
@@ -82,13 +64,13 @@ func solveSpot(unsolvedSpots []Spot, index int) bool {
 	possibleSolutions := findPossibleSolutions(unsolvedSpots[index])
 	// If we are at the last index and there is only one value -> end condition -> return true
 	if index == len(unsolvedSpots)-1 && len(possibleSolutions) == 1 {
-		puzzle[spot.row][spot.col] = possibleSolutions[0]
+		puzzle.Problem[spot.row][spot.col] = possibleSolutions[0]
 		return true
 	}
 
 	// Loop through all of those values and try them
 	for _, possibleSolution := range possibleSolutions {
-		puzzle[spot.row][spot.col] = possibleSolution
+		puzzle.Problem[spot.row][spot.col] = possibleSolution
 		// Call solveSpot(array, index + 1)
 		// if it returns true, a solution was found
 		if solveSpot(unsolvedSpots, index+1) {
@@ -97,7 +79,7 @@ func solveSpot(unsolvedSpots []Spot, index int) bool {
 	}
 
 	// Reset the spot to 0 (spot still unsolved)
-	puzzle[spot.row][spot.col] = 0
+	puzzle.Problem[spot.row][spot.col] = 0
 	return false
 
 }
@@ -107,7 +89,7 @@ func findPossibleSolutions(spot Spot) []int {
 	possibleSolutions := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 
 	// Check row
-	for _, num := range puzzle[spot.row] {
+	for _, num := range puzzle.Problem[spot.row] {
 		if num != 0 {
 			possibleSolutions = removeFromPossibleSolutions(num, possibleSolutions)
 		}
@@ -115,8 +97,8 @@ func findPossibleSolutions(spot Spot) []int {
 
 	// Check column
 	for i := 0; i < 9; i++ {
-		if puzzle[i][spot.col] != 0 {
-			possibleSolutions = removeFromPossibleSolutions(puzzle[i][spot.col], possibleSolutions)
+		if puzzle.Problem[i][spot.col] != 0 {
+			possibleSolutions = removeFromPossibleSolutions(puzzle.Problem[i][spot.col], possibleSolutions)
 		}
 	}
 
@@ -125,8 +107,8 @@ func findPossibleSolutions(spot Spot) []int {
 	y := spot.col - (spot.col % 3)
 	for i := x; i < x+3; i++ {
 		for j := y; j < y+3; j++ {
-			if puzzle[i][j] != 0 {
-				possibleSolutions = removeFromPossibleSolutions(puzzle[i][j], possibleSolutions)
+			if puzzle.Problem[i][j] != 0 {
+				possibleSolutions = removeFromPossibleSolutions(puzzle.Problem[i][j], possibleSolutions)
 			}
 		}
 	}
--- sudoku/sudoku_test.go
+++ sudoku/sudoku_test.go
@@ -1,30 +1,43 @@
 package sudoku
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSolveSudoku(t *testing.T) {
-	testProblem := `1 _ 3 _ _ 6 _ 8 _
-	_ 5 _ _ 8 _ 1 2 _
-	7 _ 9 1 _ 3 _ 5 6
-	_ 3 _ _ 6 7 _ 9 _
-	5 _ 7 8 _ _ _ 3 _
-	8 _ 1 _ 3 _ 5 _ 7
-	_ 4 _ _ 7 8 _ 1 _
-	6 _ 8 _ _ 2 _ 4 _
-	_ 1 2 _ 4 5 _ 7 8`
+	testProblemJson := `{
+		"Problem": [
+		[1,0,3,0,0,6,0,8,0],
+		[0,5,0,0,8,0,1,2,0],
+		[7,0,9,1,0,3,0,5,6],
+		[0,3,0,0,6,7,0,9,0],
+		[5,0,7,8,0,0,0,3,0],
+		[8,0,1,0,3,0,5,0,7],
+		[0,4,0,0,7,8,0,1,0],
+		[6,0,8,0,0,2,0,4,0],
+		[0,1,2,0,4,5,0,7,8]
+		],
+		"Solved": false
+		}`
 
-	expectedSolution := `1 2 3 4 5 6 7 8 9
-4 5 6 7 8 9 1 2 3
-7 8 9 1 2 3 4 5 6
-2 3 4 5 6 7 8 9 1
-5 6 7 8 9 1 2 3 4
-8 9 1 2 3 4 5 6 7
-3 4 5 6 7 8 9 1 2
-6 7 8 9 1 2 3 4 5
-9 1 2 3 4 5 6 7 8
-`
+	expectedSolutionJson := `{
+		"Problem": [
+		[1,2,3,4,5,6,7,8,9],
+		[4,5,6,7,8,9,1,2,3],
+		[7,8,9,1,2,3,4,5,6],
+		[2,3,4,5,6,7,8,9,1],
+		[5,6,7,8,9,1,2,3,4],
+		[8,9,1,2,3,4,5,6,7],
+		[3,4,5,6,7,8,9,1,2],
+		[6,7,8,9,1,2,3,4,5],
+		[9,1,2,3,4,5,6,7,8]
+		],
+		"Solved": true
+		}`
 
-	solution := SolveSudoku(testProblem)
+	expectedSolution := ConvertJsonToPuzzle(expectedSolutionJson)
+
+	solution := ConvertJsonToPuzzle(SolveSudoku(testProblemJson))
 
 	if expectedSolution != solution {
 		t.Errorf("Solution was incorrect, got:\n%v\nexpected:\n%v\n", solution, expectedSolution)
@@ -33,28 +46,39 @@ func TestSolveSudoku(t *testing.T) {
 
 //https://secure.i.telegraph.co.uk/multimedia/archive/02260/Untitled-1_2260725b.jpg
 func TestWorldsHardestSudokuProblem(t *testing.T) {
-	testProblem := `8 _ _ _ _ _ _ _ _
-	_ _ 3 6 _ _ _ _ _
-	_ 7 _ _ 9 _ 2 _ _
-	_ 5 _ _ _ 7 _ _ _
-	_ _ _ _ 4 5 7 _ _
-	_ _ _ 1 _ _ _ 3 _
-	_ _ 1 _ _ _ _ 6 8
-	_ _ 8 5 _ _ _ 1 _
-	_ 9 _ _ _ _ 4 _ _`
+	testProblemJson := `{
+		"Problem": [
+		[8,0,0,0,0,0,0,0,0],
+		[0,0,3,6,0,0,0,0,0],
+		[0,7,0,0,9,0,2,0,0],
+		[0,5,0,0,0,7,0,0,0],
+		[0,0,0,0,4,5,7,0,0],
+		[0,0,0,1,0,0,0,3,0],
+		[0,0,1,0,0,0,0,6,8],
+		[0,0,8,5,0,0,0,1,0],
+		[0,9,0,0,0,0,4,0,0]
+		],
+		"Solved": false
+		}`
 
-	expectedSolution := `8 1 2 7 5 3 6 4 9
-9 4 3 6 8 2 1 7 5
-6 7 5 4 9 1 2 8 3
-1 5 4 2 3 7 8 9 6
-3 6 9 8 4 5 7 2 1
-2 8 7 1 6 9 5 3 4
-5 2 1 9 7 4 3 6 8
-4 3 8 5 2 6 9 1 7
-7 9 6 3 1 8 4 5 2
-`
+	expectedSolutionJson := `{
+		"Problem": [
+		[8,1,2,7,5,3,6,4,9],
+		[9,4,3,6,8,2,1,7,5],
+		[6,7,5,4,9,1,2,8,3],
+		[1,5,4,2,3,7,8,9,6],
+		[3,6,9,8,4,5,7,2,1],
+		[2,8,7,1,6,9,5,3,4],
+		[5,2,1,9,7,4,3,6,8],
+		[4,3,8,5,2,6,9,1,7],
+		[7,9,6,3,1,8,4,5,2]
+		],
+		"Solved": true
+		}`
 
-	solution := SolveSudoku(testProblem)
+	expectedSolution := ConvertJsonToPuzzle(expectedSolutionJson)
+
+	solution := ConvertJsonToPuzzle(SolveSudoku(testProblemJson))
 
 	if expectedSolution != solution {
 		t.Errorf("Solution was incorrect, got:\n%v\nexpected:\n%v\n", solution, expectedSolution)
